3824933f0629d28a3e124ecbff337768462a7cdf
Amira Tarek <amirat737@gmail.com>
2025-05-04 02:03:53 +0300

    handle product categories navigation

--- customer/all_products/all_products.js
+++ customer/all_products/all_products.js
@@ -7,20 +7,51 @@ function fetchAllProducts() {
         .then(products => {
             products.sort(() => Math.random() - 0.5).forEach(product => {
                 var productCard = document.createElement('product-card');
-                productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
+                productCard.className = 'product__card col-12 col-sm-6 col-md-5 col-xl-3';
                 productCard.setAttribute('name', product.name || 'Unknown Product');
                 productCard.setAttribute('price', product.price || '0.00');
                 productCard.setAttribute('image', product.image || '');
                 productCard.setAttribute('rating', product.rating || '0');
                 productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
                 productCard.setAttribute('sale', product.sale || '');
+                productCard.setAttribute('category', product.category || '');
                 productsTitle.textContent = "All Products";
                 productsSection.appendChild(productCard);
             });
+            filterProductsByCategory();
         })
         .catch(error => console.error('Error loading JSON:', error));
 };
-// fetchAllProducts();
+fetchAllProducts();
+
+function filterProductsByCategory() {
+    const savedCategory = localStorage.getItem("category");
+    if (!savedCategory) return;
+
+    const productCards = document.querySelectorAll(".product__card");
+    let hasMatch = false;
+    productCards.forEach(card => {
+        const category = card.getAttribute("category").toLowerCase();
+        const isMatch = category === savedCategory.toLowerCase();
+        console.log(isMatch);
+        console.log(savedCategory.toLowerCase());
+        console.log(category);
+        card.style.display = isMatch ? "block" : "none";
+        if (isMatch) hasMatch = true;
+    });
+    localStorage.removeItem("category");
+
+    if (!hasMatch) {
+        document.getElementById("products-section").innerHTML = `
+            <p class="w-100 h-100">No products found</p>
+            `;
+    }
+}
+
+
+
+
+
 
 
 
@@ -73,4 +104,4 @@ function fetchFlashSalesProducts() {
         })
         .catch(error => console.error('Error loading products:', error));
 };
-fetchFlashSalesProducts();
+// fetchFlashSalesProducts();
--- customer/home/home.js
+++ customer/home/home.js
@@ -30,72 +30,79 @@ function closeOnClickOutside(event) {
 }
 
 // ------------------------------------------------------------------------------ //
+function setCategory(category) {
+  console.log('Attempting to save:', category); // Debug
+  localStorage.setItem('category', category);
+  console.log('Saved:', localStorage.getItem('category')); // Verify
+}
+
+// ------------------------------------------------------------------------------ //
 
 function fetchSliceBestSelling() {
-    var productsSection = document.getElementById('best-selling');
-    fetch('/assets/data/products.json')
-        .then(response => response.json())
-        .then(products => {
-            products.sort((a, b) => (b.rating || 0) - (a.rating || 0)).slice(0, 4).sort(() => Math.random() - 0.5).forEach(product => {
-                var productCard = document.createElement('product-card');
-                productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
-                productCard.setAttribute('name', product.name || 'Unknown Product');
-                productCard.setAttribute('price', product.price || '0.00');
-                productCard.setAttribute('image', product.image || '');
-                productCard.setAttribute('rating', product.rating || '0');
-                productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
-                productCard.setAttribute('sale', product.sale || '');
-                productsSection.appendChild(productCard);
-            });
-        })
-        .catch(error => console.error('Error loading JSON:', error));
+  var productsSection = document.getElementById('best-selling');
+  fetch('/assets/data/products.json')
+    .then(response => response.json())
+    .then(products => {
+      products.sort((a, b) => (b.rating || 0) - (a.rating || 0)).slice(0, 4).sort(() => Math.random() - 0.5).forEach(product => {
+        var productCard = document.createElement('product-card');
+        productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
+        productCard.setAttribute('name', product.name || 'Unknown Product');
+        productCard.setAttribute('price', product.price || '0.00');
+        productCard.setAttribute('image', product.image || '');
+        productCard.setAttribute('rating', product.rating || '0');
+        productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
+        productCard.setAttribute('sale', product.sale || '');
+        productsSection.appendChild(productCard);
+      });
+    })
+    .catch(error => console.error('Error loading JSON:', error));
 };
 fetchSliceBestSelling();
 
 
 function fetchSliceFlashSales() {
-    var productsSection = document.getElementById('flash-sales');
+  var productsSection = document.getElementById('flash-sales');
 
-    fetch('/assets/data/products.json')
-        .then(response => response.json())
-        .then(products => {
-            products.filter(
-                product => product.sale != null && product.sale !== ''
-            ).slice(0, 4).sort(() => Math.random() - 0.5).forEach(product => {
-                var productCard = document.createElement('product-card');
-                productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
-                productCard.setAttribute('name', product.name || 'Unknown Product');
-                productCard.setAttribute('price', product.price || '0.00');
-                productCard.setAttribute('image', product.image || '');
-                productCard.setAttribute('rating', product.rating || '0');
-                productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
-                productCard.setAttribute('sale', product.sale || '');
-                productsSection.appendChild(productCard);
-            });
-        })
-        .catch(error => console.error('Error loading JSON:', error));
+  fetch('/assets/data/products.json')
+    .then(response => response.json())
+    .then(products => {
+      products.filter(
+        product => product.sale != null && product.sale !== ''
+      ).slice(0, 4).sort(() => Math.random() - 0.5).forEach(product => {
+        var productCard = document.createElement('product-card');
+        productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
+        productCard.setAttribute('name', product.name || 'Unknown Product');
+        productCard.setAttribute('price', product.price || '0.00');
+        productCard.setAttribute('image', product.image || '');
+        productCard.setAttribute('rating', product.rating || '0');
+        productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
+        productCard.setAttribute('sale', product.sale || '');
+        productsSection.appendChild(productCard);
+      });
+    })
+    .catch(error => console.error('Error loading JSON:', error));
 };
 fetchSliceFlashSales();
 
 
 function fetchSliceAllProducts() {
-    var productsSection = document.getElementById('our-products');
-    fetch('/assets/data/products.json')
-        .then(response => response.json())
-        .then(products => {
-            products.sort(() => Math.random() - 0.5).slice(0, 4).forEach(product => {
-                var productCard = document.createElement('product-card');
-                productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
-                productCard.setAttribute('name', product.name || 'Unknown Product');
-                productCard.setAttribute('price', product.price || '0.00');
-                productCard.setAttribute('image', product.image || '');
-                productCard.setAttribute('rating', product.rating || '0');
-                productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
-                productCard.setAttribute('sale', product.sale || '');
-                productsSection.appendChild(productCard);
-            });
-        })
-        .catch(error => console.error('Error loading JSON:', error));
+  var productsSection = document.getElementById('our-products');
+  fetch('/assets/data/products.json')
+    .then(response => response.json())
+    .then(products => {
+      products.sort(() => Math.random() - 0.5).slice(0, 4).forEach(product => {
+        var productCard = document.createElement('product-card');
+        productCard.className = 'col-12 col-sm-6 col-md-5 col-xl-3';
+        productCard.setAttribute('name', product.name || 'Unknown Product');
+        productCard.setAttribute('price', product.price || '0.00');
+        productCard.setAttribute('image', product.image || '');
+        productCard.setAttribute('rating', product.rating || '0');
+        productCard.setAttribute('ratingCount', `(${product.ratingCount})` || '0');
+        productCard.setAttribute('sale', product.sale || '');
+        productsSection.appendChild(productCard);
+      });
+    })
+    .catch(error => console.error('Error loading JSON:', error));
 };
 fetchSliceAllProducts();
 
