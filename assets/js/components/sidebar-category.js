db50affbe3a97a2a03430e3e82473837f2602a1f
Amira Tarek <amirat737@gmail.com>
2025-05-04 02:32:30 +0300

    build categories reusable components

---
+++ assets/js/components/sidebar-category.js
@@ -0,0 +1,18 @@
+class SidebarCategoryComponent extends HTMLElement {
+    connectedCallback() {
+        const category = this.getAttribute('category');
+        const categoryIcon = this.getAttribute('icon');
+
+        this.innerHTML = `
+            <a href="/customer/all_products/all_products.html" class="nav-item nav-link" id="sidebarCategory">
+              ${category}
+            </a>
+        `;
+
+        this.querySelector('#sidebarCategory').addEventListener('click', (e) => {
+            window.setCategory(category);
+        });
+    }
+}
+
+customElements.define('sidebar-category', SidebarCategoryComponent);
