465d0595f6671d3aa9e2b3c4c4fba10b2e33f182
Amira Tarek <amirat737@gmail.com>
2025-05-08 00:10:03 +0300

    fetch seller products

--- seller/seller-management.js
+++ seller/seller-management.js
@@ -1,3 +1,5 @@
+import { showToast } from "../../assets/js/utils.js";
+
 export function getCurrentUser() {
   const token = localStorage.getItem("token");
   if (!token) {
@@ -18,3 +20,24 @@ if (currentUser) {
   console.log("No user logged in");
 }
 
+
+export function fetchSellerProducts() {
+  const currentUser = getCurrentUser();
+  if (!currentUser) {
+    showToast("error", "Please log in first");
+    return [];
+  }
+
+  //   if (currentUser.role !== "Seller" || !currentUser.want_to_be_seller) {
+  //     showToast("error", "Only sellers have the access");
+  //     return [];
+  //   }
+
+  const allProducts = JSON.parse(localStorage.getItem("allProducts")) || [];
+  const sellerProducts = allProducts.filter(
+    (product) => parseInt(product.createdBy) === currentUser.userId
+  );
+  console.log(sellerProducts);
+  return sellerProducts;
+}
+fetchSellerProducts();
